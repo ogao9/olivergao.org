68e5c7b73e7f73edc3c60bc976003e7fc52acdf6
Oliver Gao <ogao@umich.edu>
2023-04-30 23:30:07 -0400

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<main className="max-w-3xl mx-auto border">
+			<h1 className="text-3xl font-bold my-16">Oliver Gao</h1>
+			<p>
+				Hi! I'm Oliver. I'm a third-year student at the University of
+				Michigan studying computer science. I'm interested in building
+				beautiful technology that people love and find incredibly
+				useful.
+			</p>
+      <p>
+				Hi! I'm Oliver. I'm a third-year student at the University of
+				Michigan studying computer science. I'm interested in building
+				beautiful technology that people love and find incredibly
+				useful.
+			</p>
+      <p>
+				Hi! I'm Oliver. I'm a third-year student at the University of
+				Michigan studying computer science. I'm interested in building
+				beautiful technology that people love and find incredibly
+				useful.
+			</p>
+		</main>
+	);
+}
